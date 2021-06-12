060732f7db3ba19f3c89495c8d8e8a683381a742
pecop <back.to.the.future52@gmail.com>
2021-06-13 08:17:53 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', () => {
+        const n = Math.random();
+        if (n < 0.05) {
+            btn.textContent = '大吉';
+        } else if (n < 0.2) {
+            btn.textContent = '中吉'; 
+        } else if (n < 0.4) {
+            btn.textContent = '小吉'; 
+        } else if (n < 0.6) {
+            btn.textContent = '吉'; 
+        } else if (n < 0.8) {
+            btn.textContent = '末吉'; 
+        } else {
+            btn.textContent = '凶'; 
+        }
+    });
+}
